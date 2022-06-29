c8070898a6648f13dea7356a9a3f55332694aeb6
Michael Harris <alphasni@mail.com>
2022-06-29 18:17:54 -0500

    UN-2 Created trending headlines module

--- UptickNews/src/app/service/uptick-newsapiservice.service.ts
+++ UptickNews/src/app/service/uptick-newsapiservice.service.ts
@@ -6,14 +6,11 @@ import { Observable } from 'rxjs';
   providedIn: 'root',
 })
 export class UptickNewsapiserviceService {
-  private _http: any;
-  constructor(private _Http: HttpClient) {}
-
-  //newsapiurl
-  uptickNewsApiUrl =
+  constructor(private _http: HttpClient) {}
+  newsapiurl =
     'https://newsapi.org/v2/top-headlines?country=us&apiKey=2766ae98d4604867b2947c9ecbb6a7f3';
 
   topHeading(): Observable<any> {
-    return this._http.get(this.uptickNewsApiUrl);
+    return this._http.get(this.newsapiurl);
   }
 }
